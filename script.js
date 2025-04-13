46c3f6ac302a32d8f6ccc2d483732405caa237ec
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2025-04-13 20:38:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
     closeBtn.addEventListener('click', () => modal.style.display = 'none');
+    
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
